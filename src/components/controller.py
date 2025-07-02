9f245f2d89e9e07900f30eae25f18a30296978fb
Justin Pang <80634866+jpgit360@users.noreply.github.com>
2025-07-01 21:02:37 -0400

    fixed read/write operations (#1)
    Co-authored-by: Kenny Cui <kenny.cui@mail.utoronto.ca>

--- src/components/controller.py
+++ src/components/controller.py
@@ -20,15 +20,18 @@ class GPIOController:
         GPIO.setmode(GPIO.BCM)
         logger.info("GPIO pins setup complete.")
     
-    def read(pin_num: int, ) -> int:
+    def read(self, pin_num: int, ) -> int:
         """
         Read the value from a specified GPIO pin.
+            pin_num: BCM Pin number
         """
 
         logger.debug(f"Reading value from pin {pin_num}")
 
         try:
-            val = GPIO.input(pin_num)
+            GPIO.setup(pin_num, GPIO.IN)
+            value = GPIO.input(pin_num)
+            logger.info(f"Successfully read {value} from pin {pin_num}")
         except Exception as e:
             if isinstance(e, KeyBoardInterrupt):
                     GPIO.cleanup()
@@ -36,21 +39,39 @@ class GPIOController:
             logger.error(f"Error reading from pin {pin_num}: {e}")
             return -1
         
-        return val
+        return value
 
-    def write(pin_num: int, ) -> None:
+    def write(self, pin_num: int, value: int) -> None:
         """
         Write a value to a specified GPIO pin.
+            pin_num: BCM Pin number
+            value: 
+                GPIO.LOW
+                GPIO.HIGH
         """
+        
+        if value not in (GPIO.LOW, GPIO.HIGH):
+            raise ValueError(f"Invalid GPIO value: {value}. Must be GPIO.LOW or GPIO.HIGH.")
 
         logger.debug(f"Writing value to pin {pin_num}")
 
         try:
-            GPIO.output(pin_num, GPIO.HIGH)
-            logger.info(f"Successfully wrote to pin {pin_num}")
+            GPIO.setup(pin_num, GPIO.OUT)
+            GPIO.output(pin_num, value)
+            logger.info(f"Successfully wrote {value} to pin {pin_num}")
         except Exception as e:
             if isinstance(e, KeyBoardInterrupt):
                 GPIO.cleanup()
             
-            logger.error(f"Error writing to pin {pin_num}: {e}")
+            logger.error(f"Error writing {value} to pin {pin_num}: {e}")
+            raise e
+    
+    def cleanup(self) -> None:
+        logger.debug(f"Calling cleanup")
+
+        try:
+            GPIO.cleanup()
+            logger.info(f"Successfully called cleanup")
+        except Exception as e:            
+            logger.error(f"Error calling cleanup: {e}")
             raise e
