2beb55f1303118d390e8fa47fc063c72876abae8
Kenny Cui <kenny.cui@mail.utoronto.ca>
2025-07-01 17:57:17 -0700

    fixed read/write operations

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from logs.logging_setup import init, get_logger
 from components import GPIOController
+import RPi.GPIO as GPIO
+from time import sleep
 
 def main() -> None:
     """
@@ -10,6 +12,14 @@ def main() -> None:
     logger = get_logger(__name__)
     logger.info("Application started.")
     gpio_controller = GPIOController()
+    
+    try:
+        # Complete GPIO operations in this block
+        gpio_controller.write(27, GPIO.HIGH)
+        sleep(1)
+        gpio_controller.write(27, GPIO.LOW)
+    finally:
+        gpio_controller.cleanup()
     logger.info("Application finished.")
 
 if __name__ == "__main__":
