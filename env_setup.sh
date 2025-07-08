32e69e727b9e09da91ebe046d448cef5418988dc
Kenny Cui <kenny.cui@mail.utoronto.ca>
2025-07-08 16:54:01 -0700

    added requirements.txt

--- env_setup.sh
+++ env_setup.sh
@@ -35,3 +35,4 @@ fi
 echo "Environment setup complete. You can now run the Truss project."
 echo "To activate the virtual environment, run: source venv/bin/activate"
 echo "To deactivate the virtual environment, run: deactivate"
+echo "To install required python dependencies, run: pip install -r requirements.txt"
