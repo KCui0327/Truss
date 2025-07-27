af811fbc4512bb6eb11852908e4a102cf303fac1
KCui0327 <kenny.cui@mail.utoronto.ca>
2025-07-26 22:11:58 -0700

    Updated server

--- src/webserver/server.py
+++ src/webserver/server.py
@@ -4,7 +4,7 @@ Truss Web Server
 """
 
 from flask import Flask, request, jsonify
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
@@ -64,6 +64,7 @@ def pH():
 @socketio.on('connect')
 def webhook_stolon():
     print("Client connected")
+    send({'message': 'Welcome! You are now connected to the Truss Web Server.'})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=54321, debug=True)
