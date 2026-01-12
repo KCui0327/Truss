86ba2a86ba95b4a2095dda9a7ed10d79db0932cc
Justin Pang <justinf.pang@mail.utoronto.ca>
2026-01-12 14:58:58 -0800

    [justin_servo_dev] initial commit

---
+++ src/servo/servo_fns.py
@@ -0,0 +1,81 @@
+from servo_driver import *
+
+def getBusServoID(id=None):
+    """
+    读取串口舵机id
+    :param id: 默认为空
+    :return: 返回舵机id
+    """
+    
+    while True:
+        if id is None:  # 总线上只能有一个舵机
+            serial_servo_read_cmd(0xfe, LOBOT_SERVO_ID_READ)
+        else:
+            serial_servo_read_cmd(id, LOBOT_SERVO_ID_READ)
+        # 获取内容
+        msg = serial_servo_get_rmsg(LOBOT_SERVO_ID_READ)
+        if msg is not None:
+            return msg
+
+def getBusServoPulse(id):
+    '''
+    读取舵机当前位置
+    :param id:
+    :return:
+    '''
+    while True:
+        serial_servo_read_cmd(id, LOBOT_SERVO_POS_READ)
+        msg = serial_servo_get_rmsg(LOBOT_SERVO_POS_READ)
+        if msg is not None:
+            return msg
+
+def getBusServoTemp(id):
+    '''
+    读取舵机温度
+    :param id:
+    :return:
+    '''
+    while True:
+        serial_servo_read_cmd(id, LOBOT_SERVO_TEMP_READ)
+        msg = serial_servo_get_rmsg(LOBOT_SERVO_TEMP_READ)
+        if msg is not None:
+            return msg
+
+def getBusServoVin(id):
+    '''
+    读取舵机电压
+    :param id:
+    :return:
+    '''
+    while True:
+        serial_servo_read_cmd(id, LOBOT_SERVO_VIN_READ)
+        msg = serial_servo_get_rmsg(LOBOT_SERVO_VIN_READ)
+        if msg is not None:
+            return msg
+
+def setBusServoPulse(id, pulse, use_time):
+    """
+    驱动串口舵机转到指定位置
+    :param id: 要驱动的舵机id
+    :pulse: 位置
+    :use_time: 转动需要的时间
+    """
+
+    pulse = 0 if pulse < 0 else pulse
+    pulse = 1000 if pulse > 1000 else pulse
+    use_time = 0 if use_time < 0 else use_time
+    use_time = 30000 if use_time > 30000 else use_time
+    serial_serro_wirte_cmd(id, LOBOT_SERVO_MOVE_TIME_WRITE, pulse, use_time)
+    return pulse  #x新增反馈可以删除\
+
+def getBusServoPulse(id):
+    '''
+    读取舵机当前位置
+    :param id:
+    :return:
+    '''
+    while True:
+        serial_servo_read_cmd(id, LOBOT_SERVO_POS_READ)
+        msg = serial_servo_get_rmsg(LOBOT_SERVO_POS_READ)
+        if msg is not None:
+            return msg
