87b4f9447a5d559de27664f4c649f9ea2bd7e897
Eric Chen <73261527+3ric03@users.noreply.github.com>
2026-01-15 17:37:11 -0500

    Inverse kinematics (#24)

---
+++ src/kinematics/matlabBackend.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matlab.engine
+import os
+
+class MatlabBackend:
+    def __init__(self):
+        project_dir = os.path.dirname(__file__)
+        self.eng = matlab.engine.start_matlab()
+        self.eng.addpath(project_dir, nargout=0) # tell the engine to read matlab files in this directory
+    
+    # convert into a MATLAB suitable column vector 
+    def matlab_col(self, x):
+        return matlab.double([[v] for v in x])
+    
+    # Run forward_kinematics.m in MATLAB using RobotModel DH parameters
+    def forwardKinematics(self, q, robotModel):
+        a, alpha, d, theta_offset = robotModel.dh_table.T
+
+        q_mat = self.matlab_col(q)
+
+        H_mat = self.eng.forwardKinematics(
+            q_mat,
+            self.matlab_col(a),
+            self.matlab_col(alpha),
+            self.matlab_col(d),
+            self.matlab_col(theta_offset),
+            nargout=1
+        )
+
+        return np.array(H_mat, dtype=float)
+    
+    # Run inverse_kinematics.m in MATLAB using RobotModel DH parameters
+    def inverseKinematics(self, robotModel, H):
+        a, alpha, d, theta_offset = robotModel.dh_table.T
+        H_mat = matlab.double(H)
+        q_mat = self.eng.inverseKinematics(
+            H_mat,
+            self.matlab_col(a),
+            self.matlab_col(alpha),
+            self.matlab_col(d),
+            self.matlab_col(theta_offset),
+            nargout=1
+        )
+
+        return np.array(q_mat).flatten()
+        
+
+        
+        
---
+++ tests/test_inverse_kinematics.py
@@ -0,0 +1,28 @@
+from src.kinematics.robotModel import RobotModel 
+from src.kinematics.matlabBackend import MatlabBackend
+import math
+import numpy as np
+import logging
+
+def test_inverse_kinematics_returns_correct_joint_values():
+    robot = RobotModel(
+            a=[25, 315, 35, 0, 0, -296.23],
+            alpha=[math.pi/2, 0, math.pi/2, -math.pi/2, math.pi/2, 0],
+            d=[400, 0, 0, 365, 0, 161.44],
+            theta=[0, 0, 0, 0, 0, 0],
+            joint_limits=[[-2.094, 2.094]] * 6,
+        )
+    matlab = MatlabBackend()
+    
+    q_original = [math.pi/5, math.pi/3, -math.pi/4, math.pi/4, math.pi/3, math.pi/4]
+
+    H = matlab.forwardKinematics(q_original, robot)
+    q_computed = matlab.inverseKinematics(robot, H) 
+    
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"q_original: {q_original}")
+    logging.info(f"q_computed: {q_computed}")
+    np.testing.assert_allclose(q_original, q_computed, rtol=1e-5, atol=1e-8)
+    
+    
+test_inverse_kinematics_returns_correct_joint_values()
